ef46e0c1bb47f86b6bb59dda4e681dc445825d32
zit-lilijie <lilijie@zit.com.cn>
2020-05-11 11:47:52 +0800

    222

--- src/views/team_message/message.vue
+++ src/views/team_message/message.vue
@@ -594,7 +594,7 @@ export default {
       this.persons = [
         { personId: 1, jobNumber: "1111", personName: "aaaaa" },
         { personId: 2, jobNumber: "2222", personName: "b5bbbb" },
-        { personId: 3, jobNumber: "3333", personName: "ccccc" }
+        { personId: 3, jobNumber: "3333", personName: "cc999ccc" }
       ];
        if (this.selectedPersonIds != undefined) {
         for (var i = 0; i < this.selectedPersonIds.length; i++) {
