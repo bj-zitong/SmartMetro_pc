d5f7d7ece0c80f75a362e2a64bd2fe28cf3c3764
zit-lilijie <lilijie@zit.com.cn>
2020-05-12 11:56:28 +0800

    11

--- config/index.js
+++ config/index.js
@@ -1,42 +1,48 @@
 'use strict'
-// Template version: 1.3.1
+// Template version: 1.2.8
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path')
 
 module.exports = {
   dev: {
-    env:require('./dev.env'),
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-          // '/ajaxurl': {
-          //   target: 'http://192.168.1.164:8001',
-          //   changeOrigin: true,
-          //   pathRewrite: {
-          //     '^/ajaxurl': 'http://192.168.1.164:8001'
-          //   }
-          // }
-       '/api': {
-         target: 'http://192.168.1.100:8001', //你要跨域的网址  比如  'http://news.baidu.com',
-         // secure: true, // 如果是https接口，需要配置这个参数
-         changeOrigin: true, //这个参数是用来回避跨站问题的，配置完之后发请求时会自动修改http header里面的host，但是不会修改别的
-         pathRewrite: {
-           '^/api': 'http://192.168.1.100:8001' //路径的替换规则
-           //这里的配置是正则表达式，以/api开头的将会被用用‘/api’替换掉，假如后台文档的接口是 /api/list/xxx
-           //前端api接口写：axios.get('/api/list/xxx') ， 被处理之后实际访问的是：http://news.baidu.com/api/list/xxx
-         }
-       }
+      '/api/getmoviepiaofang': {
+        target: 'http://dianying.nuomi.com/movie/boxrefresh',
+        secure: false,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api/getmoviepiaofang': ''
+        }
+      },
+      '/api/getMovieImage': {
+        target: 'http://dianying.nuomi.com/common/ranklist',
+        secure: false,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api/getMovieImage': ''
+        }
+      },
     },
+
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
+    autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
+    // Use Eslint Loader?
+    // If true, your code will be linted during bundling and
+    // linting errors and warnings will be shown in the console.
+    useEslint: true,
+    // If true, eslint errors and warnings will also be shown in the error overlay
+    // in the browser.
+    showEslintErrorsInOverlay: false,
 
     /**
      * Source Maps
@@ -50,7 +56,7 @@ module.exports = {
     // https://vue-loader.vuejs.org/en/options.html#cachebusting
     cacheBusting: true,
 
-    cssSourceMap: true
+    cssSourceMap: true,
   },
 
   build: {
@@ -60,7 +66,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
