9e0e94c2af00c81aedcacc67196c56a8c9171b64
zit-lilijie <lilijie@zit.com.cn>
2020-05-18 09:42:07 +0800

    添加大屏跳转的方法

--- src/layout/navbar.vue
+++ src/layout/navbar.vue
@@ -108,6 +108,9 @@ export default {
     },
     logout() {
       this.$router.push({ path: "/login" });
+    },
+     qhdq(){
+        this.$router.push({ path: "/Statisticsscreen" })
     }
   }
 };
