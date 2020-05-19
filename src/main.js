a038b884ba5bec523e7ff57ffd18687ea0015b29
zit-lilijie <lilijie@zit.com.cn>
2020-05-19 14:42:31 +0800

    删除组件

--- src/main.js
+++ src/main.js
@@ -13,8 +13,6 @@ import router from './router'
 import store from './store'
 // import './utils/flexible'
 // import 'lib-flexible'
-// 自定义的全局组件
-import Notification from '@/components/notification'
 // 国际化
 import i18n from './lang'
 // 全局权限检查
@@ -37,7 +35,7 @@ Vue.use(ElementUI, {
     size: 'medium',
     i18n: (key, value) => i18n.t(key, value)
 })
-Vue.use(Notification)
+
 Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
